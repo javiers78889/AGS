f79687fc806bfd9c0c1107b7018835dcdb11405f
javiers78889 <javiers78889@gmail.com>
2024-10-29 22:47:28 -0500

    spinner

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,37 @@ import { ConteoPersonas } from './widgets/ConteoPersonas'
 import { Planes } from './widgets/Planes'
 import { Recomended } from './widgets/Recomended'
 import { Servicios } from './widgets/Servicios'
+import { useEffect, useState } from 'react'
+import ags from './img/ags.png'
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Carga durante 3 segundos
+
+    return () => clearTimeout(timer); // Limpiar el timer al desmontar
+  }, []);
 
   return (
     <>
-     <Heroe/>
-     <Slider/>
-     <Servicios/>
-     <Planes/>
-     <ConteoPersonas/>
-     <Recomended/>
-     <Footer/>
+      {isLoading ? (
+        <div className="flex items-center justify-center min-h-screen ">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500 border-t-transparent"><img src={ags} alt="" /></div>
+        </div>
+      ) : (
+        <div>
+          <Heroe />
+          <Slider />
+          <Servicios />
+          <Planes />
+          <ConteoPersonas />
+          <Recomended />
+          <Footer />
+        </div>
+      )}
     </>
   )
 }
