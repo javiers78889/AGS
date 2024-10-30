367a278997dbc53dc23abbe8034e0c8cd0763c3b
javiers78889 <javiers78889@gmail.com>
2024-10-29 19:00:14 -0500

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Footer } from './components/Footer'
+import { Heroe } from './components/Heroe'
+import { Slider } from './components/Slider'
+import { Recomended } from './widgets/Recomended'
+import { Servicios } from './widgets/Servicios'
+
+function App() {
+
+  return (
+    <>
+     <Heroe/>
+     <Slider/>
+     <Servicios/>
+     <Recomended/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
